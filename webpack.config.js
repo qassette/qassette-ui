a1034f4c16b1e2c518c34d908128ed22864f1ba3
Shkrt <zxcgpppmnn@gmail.com>
2020-01-28 20:37:03 +0300

    Fix indentation

--- webpack.config.js
+++ webpack.config.js
@@ -5,28 +5,27 @@ const path = require('path');
 const mode = process.env.NODE_ENV || 'development';
 const prod = mode === 'production';
 
-
 module.exports = {
-	entry: {
-		bundle: ['./src/main.js']
-	},
-	resolve: {
-		alias: {
-			svelte: path.resolve('node_modules', 'svelte')
-		},
-		extensions: ['.mjs', '.js', '.svelte'],
-		mainFields: ['svelte', 'browser', 'module', 'main']
-	},
-	output: {
-		path: __dirname + '/public',
-		filename: '[name].js',
-		chunkFilename: '[name].[id].js'
-	},
-	module: {
-		rules: [
-			{
-				test: /\.svelte$/,
-				use: [
+  entry: {
+    bundle: ['./src/main.js']
+  },
+  resolve: {
+    alias: {
+      svelte: path.resolve('node_modules', 'svelte')
+    },
+    extensions: ['.mjs', '.js', '.svelte'],
+    mainFields: ['svelte', 'browser', 'module', 'main']
+  },
+  output: {
+    path: __dirname + '/public',
+    filename: '[name].js',
+    chunkFilename: '[name].[id].js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.svelte$/,
+        use: [
           {
             loader: 'svelte-loader',
             options: {
@@ -35,34 +34,34 @@ module.exports = {
             }
           },
         ]
-			},
-			{
-				test: /\.scss$/,
-				use: [
-					prod ? MiniCssExtractPlugin.loader : 'style-loader',
-					'css-loader',
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          prod ? MiniCssExtractPlugin.loader : 'style-loader',
+          'css-loader',
           'sass-loader'
-				]
-			},
-			{
-				test: /\.css$/,
-				use: [
-					prod ? MiniCssExtractPlugin.loader : 'style-loader',
-					'css-loader'
-				]
-			}
-		]
-	},
-	mode,
+        ]
+      },
+      {
+        test: /\.css$/,
+        use: [
+          prod ? MiniCssExtractPlugin.loader : 'style-loader',
+          'css-loader'
+        ]
+      }
+    ]
+  },
+  mode,
   plugins: [
     new webpack.DefinePlugin({
       'process.env': {
         'APP_URL': "'http://localhost:2300'"
       }
     }),
-		new MiniCssExtractPlugin({
-			filename: '[name].css'
-		})
-	],
-	devtool: prod ? false: 'source-map'
+    new MiniCssExtractPlugin({
+      filename: '[name].css'
+    })
+  ],
+  devtool: prod ? false: 'source-map'
 };
